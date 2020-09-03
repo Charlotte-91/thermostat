ef831818e6986381d62265a44fbb9008cebdbcbe
Iliyan-Y <maximizebg@gmail.com>
2020-09-03 19:53:41 +0300

    Get weather from the api

--- src/interface.js
+++ src/interface.js
@@ -33,6 +33,14 @@ $(document).ready(function () {
     colours();
   });
 
+  $.get(
+    "http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric",
+    function (data) {
+      $("#location-temperature").text(`${data.main.temp} C`);
+      $("#location-name").text(data.name);
+    }
+  );
+
   function colours() {
     if (thermostat.usageText == "low-usage") {
       $("body").css("background", "lightblue");
