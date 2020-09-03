c5802c8ec67df17cd20e38b82ed63d5338fe183d
Iliyan-Y <maximizebg@gmail.com>
2020-09-03 17:38:57 +0300

    Max temperature and power saving mode added

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,22 +1,45 @@
-'use strict;'
+"use strict;";
 
-describe('Thermostat', function() {
-  var thermostat; 
+describe("Thermostat", function () {
+  var thermostat;
 
-  beforeEach(function() {
+  beforeEach(function () {
     thermostat = new Thermostat();
   });
 
-  it('starts at 20 degrees', function() {
+  it("starts at 20 degrees", function () {
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
 
-  it('increases in temperature with up()', function() {
+  it("increases in temperature with up()", function () {
     thermostat.up();
     expect(thermostat.getCurrentTemperature()).toEqual(21);
   });
-  it('decreases in temperature with up()', function() {
+  it("decreases in temperature with up()", function () {
     thermostat.down();
     expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  it("Has default min temperature of 10 degrees", function () {
+    for (var i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
+  it("Has maximum temperature of 25 in power saving mode", function () {
+    for (var i = 0; i < 8; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(25);
+  });
+
+  it("Has maximum temperature of 32 in normal saving mode", function () {
+    thermostat.powerSaving();
+    for (var i = 0; i < 25; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(32);
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,18 +1,46 @@
-'use strict;'
+"use strict;";
 
 class Thermostat {
- constructor() {
-   this.temperature = 20;
- }
+  constructor() {
+    this.temperature = 20;
+    this.MINIMUM_TEMPERATURE = 10;
+    this.maxTemp = 25;
+    this.powerSavingMode = true;
+  }
 
- getCurrentTemperature(){
-   return this.temperature;
- }
+  getCurrentTemperature() {
+    return this.temperature;
+  }
 
- up() {
-   return this.temperature += 1
- }
- down(){
-   return this.temperature -= 1
- }
-};
+  up() {
+    if (this.isMaximumTemp()) {
+      return;
+    }
+    return (this.temperature += 1);
+  }
+  down() {
+    if (this.isMinimumTemperature()) {
+      return;
+    }
+    return (this.temperature -= 1);
+  }
+
+  isMinimumTemperature() {
+    return this.temperature === this.MINIMUM_TEMPERATURE;
+  }
+
+  isMaximumTemp() {
+    if (this.powerSavingMode) {
+      this.maxTemp = 25;
+      return this.temperature === this.maxTemp;
+    }
+    this.maxTemp = 32;
+    return this.temperature === this.maxTemp;
+  }
+
+  powerSaving() {
+    this.powerSavingMode === true
+      ? (this.powerSavingMode = false)
+      : (this.powerSavingMode = true);
+  }
+}
